ceb5f7d46ba7182ce8ecf0585159bd3eae09403e
Dwight <d.perello@outlook.com>
2024-07-15 18:31:42 +0800

    test

--- app/src/main/java/com/example/devopsapplicatioin/presenter/Home/HomeFragment.kt
+++ app/src/main/java/com/example/devopsapplicatioin/presenter/Home/HomeFragment.kt
@@ -38,7 +38,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Analytics.trackEvent("HOME FRAGMENT STARTED FINALsssss")
+        Analytics.trackEvent("HOME FRAGMENT STARTED FINALssssss")
     }
 
     override fun onDestroyView() {
